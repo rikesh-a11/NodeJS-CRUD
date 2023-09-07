a0a00c7c81eb3dd51712fb88340694dac4cbb5bd
rikesh-a11 <rikeshshrestha499@gmail.com>
2023-09-07 16:11:35 +0545

    Post Method

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express = require("express") //requiring express package
 const app = express()       //storing it in app, app vanne variable throughout use garinxa
 
+// ejs use garna lako , kk chainey ho env set gardey
+app.set("view engine","ejs")
 
+//form bata data aairaxa parse or habdle gar vaneko
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//AllBlogs
 app.get('/',(req,res)=>{
-    res.send("<h1>home page hi</h1>")
-})
-1
-app.get("/about",(req,res)=>{
-    res.send("About page")
+    // console.log(req)  
+    res.render("blogs")
 })
 
+//createBlog
+app.get('/createBlog',(req,res)=>{
+    res.render('createBlog')
+})
 
+//createBlog Post
+app.post('/createBlog',(req,res)=>{
+    console.log(req.body.title)
+    res.send('Form submitted successfully')
+})
 
 
 
 //port number (0-1100 for internal and 1100-6555 for services)
-app.listen(3000,function(){
+app.listen(3000,()=>{
     console.log("NodeJs Project has started on port 3000")
 })
